2a3963661759ba040dd5884d5c64ab408f4f820b
globalsmile <mohammedbadmus823@gmail.com>
2023-07-19 23:55:57 +0100

    added: sample.c file

---
+++ sample.c
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Return: This program prints all the values of a char.
+ * It is an example of a program that will run forever.
+ */
+int main(void)
+{
+        char c;
+
+        c = CHAR_MIN;
+        while (c != CHAR_MAX)
+        {
+            printf("%d\n", c);
+            c = c + 1;
+        }
+        return (EXIT_SUCCESS);
+}
