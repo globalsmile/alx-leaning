d818e105725f772ef9e32192aad3ff9306026cd3
globalsmile <mohammedbadmus823@gmail.com>
2023-08-28 03:18:48 -0700

    add: 1-isdigit.c file

---
+++ alx-low_level_programming/0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - determine if digit
+ * @c: character to determine
+ * Return: 1 if digit, 0 otherwise
+ *
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
